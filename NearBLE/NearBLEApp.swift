e45969996af90fcbb77951db9dc94b72fff46c82
Adis Mulabdic <adis.mulabdic@gmail.com>
2026-03-17 12:47:35 +0100

    Initial Commit

---
+++ NearBLE/NearBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearBLEApp.swift
+//  NearBLE
+//
+//  Created by Adis Mulabdic on 17. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct NearBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
